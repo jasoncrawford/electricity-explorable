cdece5690b69103ecf11596c6f41e09ddec058ee
Jason Crawford <jason@jasoncrawford.org>
2019-12-15 21:30:46 -0800

    Add random customers to model and diagram

--- src/DiagramView.jsx
+++ src/DiagramView.jsx
@@ -4,6 +4,8 @@ import { observer } from "mobx-react";
 
 @observer
 export class DiagramView extends React.Component {
+  scale = 10; // px / km
+
   @observable bounds;
 
   @computed get model() {
@@ -22,12 +24,17 @@ export class DiagramView extends React.Component {
     if (this.svg) this.bounds = this.svg.getBoundingClientRect();
   }
 
+  renderCustomers() {
+    return this.model.customers.map(c => <circle cx={c.x * this.scale} cy={c.y * this.scale} r="7" fill="#999" />);
+  }
+
   render() {
     return (
       <svg ref={el => (this.svg = el)} className="diagram">
         <g transform={`translate(${this.width / 2} ${this.height / 2})`}>
           <rect x="-25" y="-25" width="50" height="50" stroke="#999" fill="none" />
-          <circle cx="0" cy="0" r={10 * this.model.radius} stroke="#999" fill="none" />
+          <circle cx="0" cy="0" r={this.model.radius * this.scale} stroke="#999" fill="none" />
+          {this.renderCustomers()}
         </g>
       </svg>
     );
--- src/Model.js
+++ src/Model.js
@@ -1,5 +1,24 @@
 import { observable } from "mobx";
+const { random, cos, sin, PI } = Math;
 
 export class Model {
-  @observable radius = 20
+  @observable radius = 20;
+
+  numCustomers = 150;
+  customers = [];
+
+  constructor() {
+    for (let i = 0; i < this.numCustomers; i++) {
+      this.customers[i] = this.makeRandomCustomer();
+    }
+  }
+
+  makeRandomCustomer() {
+    let r = 5 + random() * 55;
+    let theta = random() * 2 * PI;
+    return {
+      x: r * cos(theta),
+      y: r * sin(theta)
+    };
+  }
 }
