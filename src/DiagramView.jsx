4a6c14c7dbc75bc990e3421c7b1d9aef6fe660d2
Jason Crawford <jason@jasoncrawford.org>
2019-12-15 17:08:43 -0800

    Draw a circle on the diagram

--- src/DiagramView.jsx
+++ src/DiagramView.jsx
@@ -2,6 +2,8 @@ import * as React from "react"
 
 export class DiagramView extends React.Component {
   render() {
-    return <svg class="diagram"></svg>
+    return <svg class="diagram">
+      <circle cx="50%" cy="50%" r="200" stroke="#999" fill="none" />
+    </svg>
   }
 }
