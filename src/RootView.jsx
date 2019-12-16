09bef391d190d491433c89792eeddd6f74bed783
Jason Crawford <jason@jasoncrawford.org>
2019-12-15 16:09:54 -0800

    Add a stub RootView

---
+++ src/RootView.jsx
@@ -0,0 +1,10 @@
+import * as React from "react"
+
+export class RootView extends React.Component {
+  render () {
+    return <>
+      <h1>Electricity Explorable</h1>
+      <p>Watch this space</p>
+    </>
+  }
+}
--- src/main.js
+++ src/main.js
@@ -1,1 +1,9 @@
-console.log("hello, world!")
+import * as React from "react"
+import * as ReactDOM from "react-dom"
+
+import { RootView } from "./RootView"
+
+console.log("Electricity Explorable v0.1 booting up")
+
+const root = document.getElementById('root')
+ReactDOM.render(<RootView />, root)
