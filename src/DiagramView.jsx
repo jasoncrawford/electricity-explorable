962f4479d8ee6f24aa03653cef84ae3a4e58889b
Jason Crawford <jason@jasoncrawford.org>
2019-12-15 19:13:00 -0800

    Add box in the middle of the diagram

--- src/DiagramView.jsx
+++ src/DiagramView.jsx
@@ -1,18 +1,45 @@
-import * as React from "react"
-import { computed } from "mobx";
+import * as React from "react";
+import { computed, observable } from "mobx";
 import { observer } from "mobx-react";
 
 @observer
 export class DiagramView extends React.Component {
+  @observable bounds;
+
   @computed get model() {
     return this.props.model;
   }
 
+  @computed get width() {
+    return (this.bounds && this.bounds.width) || 100;
+  }
+
+  @computed get height() {
+    return (this.bounds && this.bounds.height) || 100;
+  }
+
+  resize() {
+    if (this.svg) this.bounds = this.svg.getBoundingClientRect();
+  }
+
   render() {
     return (
-      <svg className="diagram">
-        <circle cx="50%" cy="50%" r={10 * this.model.radius} stroke="#999" fill="none" />
+      <svg ref={el => (this.svg = el)} className="diagram">
+        <g transform={`translate(${this.width / 2} ${this.height / 2})`}>
+          <rect x="-25" y="-25" width="50" height="50" stroke="#999" fill="none" />
+          <circle cx="0" cy="0" r={10 * this.model.radius} stroke="#999" fill="none" />
+        </g>
       </svg>
-    )
+    );
+  }
+
+  componentDidMount() {
+    this.resizer = this.resize.bind(this);
+    window.addEventListener("resize", this.resizer);
+    this.resize(); // need to resize immediately because we couldn't get the bounds until after initial render
+  }
+
+  componentWillUnmount() {
+    window.removeEventListener("resize", this.resizer);
   }
 }
